0be46e58ac774a868c7cb7597352eeca9ba36b99
Mari <klp-2009@mail.ru>
2022-07-24 18:07:21 +0300

    Добавьте файлы проекта.

---
+++ RubCase/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RubCase
+{
+    public class RubCase
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            string s = "рублей";
+            if (n % 10 == 1) s = "рубль";
+            if (n % 10 >= 2 && n % 10 <= 4) s = "рубля";
+            if (n % 100 >= 11 & n % 100 <= 20) s = "рублей";
+            Console.WriteLine("{0} {1}", n, s);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RubCaseTest/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace RubCaseTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("рублей", new RubCase.RubCase().  );
+        }
+    }
+}
